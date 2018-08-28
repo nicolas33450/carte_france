e09b3f6d4c005b15bebc94d4939d331944b7b3ab
nicolas33450 <40290258+nicolas33450@users.noreply.github.com>
2018-08-28 16:30:02 +0200

    Initial commit

---
+++ affiche_ville.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+        <title> affiche ville </title>
+    </head>
+
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 text-center p-5">
+                    <img src="carte_france.php" alt="france" title="france" usemap="#plan"/>
+                </div>
+            </div>
+        </div>
+        
+
+        <map id="plan" name="plan">
+
+            <?php
+            require 'connexion.php';
+            $requete = $pdo->prepare('select * from ville');
+            $requete->execute();
+
+            while($ligne_tab = $requete->fetch())
+            {
+            ?>
+
+            <area shape="circle" coords="<?=$ligne_tab['pos_x']?>,<?=$ligne_tab['pos_y']?>,<?=$ligne_tab['diametre']?>"
+                  href="<?= $ligne_tab['url']?>"
+                  target="_blank" alt="MDN" />
+
+            <?php
+            }
+            ?>
+        </map>
+
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
+    </body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ carte_france.php
@@ -0,0 +1,31 @@
+<?php
+require 'connexion.php';
+
+header("Content-type: image/jpeg");
+// chargement de l'image
+$carte = imagecreatefromjpeg("france.jpg");
+
+
+
+
+$requete = $pdo->prepare('select * from ville');
+$requete->execute();
+
+while($ligne_tab = $requete->fetch())
+{         
+    $noir = imagecolorallocate($carte, mt_rand(0,255), mt_rand(0,255), mt_rand(0,255));
+    //x  y   diam
+    imagefilledellipse($carte, $ligne_tab['pos_x'], $ligne_tab['pos_y'], $ligne_tab['diametre'],$ligne_tab['diametre'], $noir);
+
+    //imagettftext (image destination, taille, inclinaison, pos_x, pos_y, couleur, chemin police ttf, texte a écrire);
+    imagettftext($carte, 20, mt_rand(1,360), $ligne_tab['pos_x'] + 15, $ligne_tab['pos_y'] + 5, $noir, "./desyrel.ttf", $ligne_tab['ville']);  
+    
+    
+
+}
+
+// creation de l'image au vol
+imagejpeg($carte);
+?>
+
+
